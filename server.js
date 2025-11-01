32fcba426820af2c24e671034439f2cfef406f8b
abhirami-senthilkumar <sabhirami.saitech@gmail.com>
2025-11-01 18:14:25 +0530

    image updated

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const __dirname = path.dirname(__filename);
 
 // ✅ Serve uploads folder statically
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-app.use("/Static-Image", express.static(path.join(__dirname, "Static-Image")));
+app.use("/static-image", express.static(path.join(__dirname, "static-image")));
 
 // const PORT = 5000;
 const PORT = process.env.PORT || 5000;
@@ -43,8 +43,8 @@ app.post("/api/tryon", async (req, res) => {
         fs.writeFileSync(uploadPath, Buffer.from(base64Data, "base64"));
 
         // ✅ Create a local URL accessible by LightX API
-        // const localUrl = `http://localhost:${PORT}/uploads/${filename}`;
-        const localUrl = `${process.env.BASE_URL || "https://virtual-fit.onrender.com"}/uploads/${filename}`;
+        const localUrl = `http://localhost:${PORT}/uploads/${filename}`;
+        // const localUrl = `${process.env.BASE_URL || "https://virtual-fit.onrender.com"}/uploads/${filename}`;
 
         /* const localUrl = "https://images.pexels.com/photos/1580271/pexels-photo-1580271.jpeg";
         const styleImageUrl = "https://images.pexels.com/photos/291759/pexels-photo-291759.jpeg"; */
